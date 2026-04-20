e7893d52ec8eef379a6a9c1ae8c6600bab4bd125
Ilya Storozhilov <storozh94@gmail.com>
2026-04-21 01:20:38 +0700

    тесты первого урока, conftest

---
+++ test_fixtures.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture
+def login_page(browser):
+    pass
+
+@pytest.fixture
+def user():
+    print('Юзер')
+    return "username", "password"
+
+
+
+def test_login(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
